cf58cf912f910752f11b1c8c8eb5783f875b7104
F1ReKing <284061805@qq.com>
2017-09-25 10:21:25 +0800

    1. 增加自定义属性

--- library/src/main/java/com/f1reking/library/GradientTextView.java
+++ library/src/main/java/com/f1reking/library/GradientTextView.java
@@ -2,6 +2,7 @@ package com.f1reking.library;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.LinearGradient;
@@ -9,6 +10,7 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.Shader;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.widget.TextView;
 
 /**
@@ -19,10 +21,18 @@ import android.widget.TextView;
 
 public class GradientTextView extends TextView {
 
+    private static final String TAG = GradientTextView.class.getSimpleName();
+
     private LinearGradient mLinearGradient;
     private Paint mPaint;
     private Rect mTextBound = new Rect();
     private Rect mLastTextBound = new Rect();
+    //渐变色长度
+    private int gradientLength = 3;
+    //渐变开始颜色
+    private int startColor = Color.WHITE;
+    //渐变结束颜色
+    private int endColor = Color.TRANSPARENT;
 
     public GradientTextView(Context context) {
         this(context, null);
@@ -34,6 +44,11 @@ public class GradientTextView extends TextView {
 
     public GradientTextView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.GradientTextView);
+        gradientLength = typedArray.getInteger(R.styleable.GradientTextView_gradientLength, gradientLength);
+        startColor = typedArray.getColor(R.styleable.GradientTextView_startColor, startColor);
+        endColor = typedArray.getColor(R.styleable.GradientTextView_endColor, endColor);
+        typedArray.recycle();
     }
 
     @SuppressLint("DrawAllocation")
@@ -42,11 +57,11 @@ public class GradientTextView extends TextView {
         super.onDraw(canvas);
         mPaint = getPaint();
         String content = getText().toString();
-        if (content.length() > 4) {
+        if (content.length() > gradientLength + 1) {
             mPaint.getTextBounds(content, 0, content.length(), mTextBound);
-            mPaint.getTextBounds(content, content.length() - 3, content.length(), mLastTextBound);
+            mPaint.getTextBounds(content, content.length() - gradientLength, content.length(), mLastTextBound);
             mLinearGradient = new LinearGradient(mTextBound.width() - mLastTextBound.width(), 0, mTextBound.width(), 0,
-                new int[] { Color.WHITE, Color.TRANSPARENT }, null, Shader.TileMode.CLAMP);
+                new int[] { startColor, endColor }, null, Shader.TileMode.CLAMP);
             mPaint.setShader(mLinearGradient);
             canvas.drawText(content, getMeasuredWidth() / 2 - mTextBound.width() / 2,
                 getMeasuredHeight() + mTextBound.height(), mPaint);
